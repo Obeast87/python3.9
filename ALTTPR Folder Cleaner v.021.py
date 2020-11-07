551c1ab4046d27a9bc0eb787c71e261977c52ad8
David Hendren <obeast@gmail.com>
2020-11-07 13:03:00 -0800

    Add files via upload

---
+++ ALTTPR Folder Cleaner v.021.py
@@ -0,0 +1,45 @@
+#Deletes or trashes the ALTTPR folder and all it's contents (seeds/ROMs, screenshots, etc)
+
+#Possilble Future Changes:
+#Change it (or give an option) to only delete *.sfc files in said folder
+#Consolidate both capital and lowercase commands to save space
+#Show how many files were deleted/trashed
+
+#Imported modules
+import os
+import shutil
+from send2trash import send2trash
+
+#Intro message
+print("""The Legend of Zelda: A Link to the Past Randomizer (ALTTPR) Folder Cleaner v.021
+""")
+
+#Input prompt
+trash = input('[D]elete or [T]rash the folder?: ')
+
+#Trash
+if trash == ("t"):
+    send2trash("/Users/obeast/Downloads/ROMs/SNES/ALTTPR")
+    os.mkdir("/Users/obeast/Downloads/ROMs/SNES/ALTTPR")
+    print("ALTTPR folder trashed!")
+
+elif trash == ("T"):
+    send2trash("/Users/obeast/Downloads/ROMs/SNES/ALTTPR")
+    os.mkdir("/Users/obeast/Downloads/ROMs/SNES/ALTTPR")
+    print("ALTTPR folder trashed!")
+
+#Delete
+elif trash == ("d"):
+    shutil.rmtree("/Users/obeast/Downloads/ROMs/SNES/ALTTPR")
+    os.mkdir("/Users/obeast/Downloads/ROMs/SNES/ALTTPR")
+    print("ALTTPR folder deleted!")
+
+elif trash == ("D"):
+    shutil.rmtree("/Users/obeast/Downloads/ROMs/SNES/ALTTPR")
+    os.mkdir("/Users/obeast/Downloads/ROMs/SNES/ALTTPR")
+    print("ALTTPR folder deleted!")
+
+#Invalid input message
+else:
+    print("""Invalid Input. 
+Exiting Program.""")
